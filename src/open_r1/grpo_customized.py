116b0e1bb7318ddb82fc2ddb0f33a6732d98140c
billcai <billcaiy@amazon.com>
2025-04-21 07:41:51 +0000

    remove use_cache

--- src/open_r1/grpo_customized.py
+++ src/open_r1/grpo_customized.py
@@ -171,7 +171,6 @@ def main(script_args, training_args, model_args):
     if trainer.accelerator.is_main_process:
         trainer.create_model_card(**kwargs)
         # Restore k,v cache for fast inference
-        trainer.model.config.use_cache = True
         trainer.model.config.save_pretrained(training_args.output_dir)
 
     ##########
